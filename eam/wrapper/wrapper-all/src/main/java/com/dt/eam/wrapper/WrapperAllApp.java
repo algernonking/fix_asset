920cf02e1dd017d9e5d1830b3a008237fb1c2b75
lifangjie <leefangjie@qq.com>
2021-06-10 16:20:19 +0800

    依赖升级

--- eam/wrapper/wrapper-all/src/main/java/com/dt/eam/wrapper/WrapperAllApp.java
+++ eam/wrapper/wrapper-all/src/main/java/com/dt/eam/wrapper/WrapperAllApp.java
@@ -14,6 +14,7 @@ import com.dt.eam.misc.EamMeta;
 })
 @SpringBootApplication(exclude = {FeignAutoConfiguration.class})
 public class WrapperAllApp {
+
 	public static void main(String[] args) {
 		BootApplication.run(WrapperAllApp.class, args);
 	}
