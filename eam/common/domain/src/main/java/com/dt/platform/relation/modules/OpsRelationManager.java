0affab5bcfd0a799f024d510646326493ba97579
Lank <maillank@qq.com>
2021-09-26 21:15:50 +0800

    完善功能

--- eam/common/domain/src/main/java/com/dt/platform/relation/modules/OpsRelationManager.java
+++ eam/common/domain/src/main/java/com/dt/platform/relation/modules/OpsRelationManager.java
@@ -15,7 +15,7 @@ import org.github.foxnic.web.domain.system.UserTenant;
 
 import java.util.Iterator;
 
-public class OpsRelationManager extends RelationManager {
+public class OPSRelationManager extends RelationManager {
     @Override
     protected void config() {
         this.setupRelations();
