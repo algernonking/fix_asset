4e5db09d607d543d3c29375faa8483636aa703f1
Lank <maillank@qq.com>
2021-11-05 20:46:40 +0800

    完善功能

--- eam/common/framework/src/main/java/com/dt/platform/framework/datasource/EncryptUtil.java
+++ eam/common/framework/src/main/java/com/dt/platform/framework/datasource/EncryptUtil.java
@@ -8,11 +8,11 @@ import java.io.File;
 public class EncryptUtil {
 
     public static void main(String[] args) {
-        String passwd= FileUtil.readText(new File("/tmp/foxnic/passwd.txt"));
+        String passwd= FileUtil.readText(new File("/Users/Shared/foxnic/passwd.txt"));
         AESUtil aes=new AESUtil(passwd);
         String user="root";
-        String pwd="11111";
-        String url="jdbc:mysql://39.105.191.22:3306/eam?useSSL=false&serverTimezone=Hongkong&useUnicode=true&characterEncoding=utf-8&autoReconnect=true&allowPublicKeyRetrieval=true&tinyInt1isBit=false";
+        String pwd="123456";
+        String url="jdbc:mysql://121.43.103.102:3306/eam?useSSL=false&serverTimezone=Hongkong&useUnicode=true&characterEncoding=utf-8&autoReconnect=true&allowPublicKeyRetrieval=true&tinyInt1isBit=false";
         String url2="jdbc:mysql://39.105.191.22:3306/eam_demo?useSSL=false&serverTimezone=Hongkong&useUnicode=true&characterEncoding=utf-8&autoReconnect=true&allowPublicKeyRetrieval=true&tinyInt1isBit=false";
         user=aes.encryptData(user);
         pwd=aes.encryptData(pwd);
