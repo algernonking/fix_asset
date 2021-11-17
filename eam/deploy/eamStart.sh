01bfb5029793bd59b05404a6870e7265382b354c
Lank <maillank@qq.com>
2021-11-17 15:41:02 +0800

    完善功能11

--- eam/deploy/eamStart.sh
+++ eam/deploy/eamStart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cur_dir=$(cd `dirname $0`; pwd)
 cd $cur_dir/bin
-sh eam.sh start
+sh app.sh start
 exit 0
--- eam/deploy/eamStop.sh
+++ eam/deploy/eamStop.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cur_dir=$(cd `dirname $0`; pwd)
 cd $cur_dir/bin
-sh eam.sh stop
+sh app.sh stop
 exit 0
