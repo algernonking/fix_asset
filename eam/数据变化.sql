0970fb188b50e4272c10a016d31d6a16adaecae1
lankl <maillank@qq.com>
2022-03-28 21:29:17 +0800

    完善功能

--- eam/数据变化.sql
+++ eam/数据变化.sql
@@ -1,4 +1,5 @@
 Version 1.0.8
+
 ALTER TABLE `eam_asset_repair` ADD `report_user_name` VARCHAR(500)  NULL  DEFAULT NULL  COMMENT '报修人'  AFTER `report_user_id`;
 --update eam_asset_repair set report_user_name=report_user_id;
 
